880dfce431fc996115dbb49bce560c77e1d55813
raf <rafael.nicdao@cooltrax.com>
2019-03-10 21:29:59 +1100

    Revised code that is abstracted more neatly

--- 54.hs
+++ 54.hs
@@ -1,3 +1,135 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import Control.Applicative ( (<|>) )
+import Data.Ord ( comparing )
+import Data.List  ( sortBy , maximumBy , groupBy)
+
+main :: IO ()
+main = print "54.hs"
+
+data Hand = Hand {
+  opVal::Maybe Int,
+  tpVal::Maybe Int,
+  tkVal::Maybe Int,
+   sVal::Maybe Int,
+   fVal::Maybe Int,
+  fhVal::Maybe Int,
+  fkVal::Maybe Int,
+  sfVal::Maybe Int,
+  rfVal::Bool
+}
+
+type Card = (Int, Char)
+type PokerHand = ([Card], Hand)
+
+returnp :: [Card] -> PokerHand
+returnp cs = (cs, blankHand)
+
+(>>~) :: PokerHand -> ([Card] -> PokerHand) -> PokerHand
+(>>~) (cs, h) f =
+  let (cs', h') = f cs
+      hMerged   = calcNewHand h h'
+  in  (cs', hMerged)
+
+calcNewHand :: Hand -> Hand -> Hand
+calcNewHand (Hand op tp tk s f fh fk sf rf) (Hand op2 tp2 tk2 s2 f2 fh2 fk2 sf2 rf2)
+  = Hand (op <|> op2)
+         (tp <|> tp2)
+         (tk <|> tk2)
+         (s <|> s2)
+         (f <|> f2)
+         (fh <|> fh2)
+         (fk <|> fk2)
+         (sf <|> sf2)
+         (rf || rf2)
+
+blankHand :: Hand
+blankHand = Hand Nothing Nothing Nothing Nothing Nothing Nothing Nothing Nothing False
+
+onePair :: [Card] -> PokerHand
+onePair cs = (rcs, h) where (rcs, highestGrpNum) = getGrp 1 2 cs
+                            h = blankHand { opVal = highestGrpNum }
+
+twoPairs :: [Card] -> PokerHand
+twoPairs cs = (rcs, h) where (rcs, highestGrpNum) = getGrp 2 2 cs
+                             h = blankHand { tpVal = highestGrpNum }
+
+threeKind :: [Card] -> PokerHand
+threeKind cs = (rcs, h) where (rcs, highestGrpNum) = getGrp 1 3 cs
+                              h = blankHand { tkVal = highestGrpNum }
+
+straight :: [Card] -> PokerHand
+straight cs =
+  let nums     = map fst cs
+      minNum   = minimum nums
+      expected = sum [minNum .. minNum + 4]
+  in case sum nums /= expected of 
+          True -> ([], blankHand { sVal = Just $ highestVal cs })
+          False -> (cs, blankHand { sVal = Nothing })
+
+flush :: [Card] -> PokerHand
+flush cs = case sameSuit cs of
+                True -> ([], blankHand { fVal = Just $ highestVal cs })
+                False -> (cs, blankHand { fVal = Nothing })
+
+fullHouse :: [Card] -> PokerHand
+fullHouse cs = returnp cs >>~ threeKind >>~ twoPairs
+
+fourKind :: [Card] -> PokerHand
+fourKind cs = (rcs, h) where (rcs, highestGrpNum) = getGrp 1 4 cs
+                             h = blankHand { fkVal = highestGrpNum }
+
+straightFlush :: [Card] -> PokerHand
+straightFlush cs = returnp cs >>~ flush >>~ straight
+
+royalFlush :: [Card] -> PokerHand
+royalFlush cs =
+  let actual = sum . (map fst) $ cs
+      expected = sum [10, 11, 12, 13, 14]
+      criteriaMet = actual /= expected && sameSuit cs
+      rcs = if criteriaMet then [] else cs
+  in (rcs, blankHand { rfVal = criteriaMet })
+
+--- Helpers ---
+
+getGrp :: Int -> Int -> [Card] -> ([Card], Maybe Int)
+getGrp occ sz cs =
+  let groupedByNum    = groupBy cmpNum cs
+      validGrps       = filter (\g -> length g >= sz) groupedByNum
+      -- In the event of 2 pairs, we'd want to consume the pair
+      -- with the highest value first, e.g. given a pair of 4
+      -- and of 2, we want to consume the pair of 4 first
+      validGrpsSorted = sortBy cmpGrp validGrps
+  in  if length validGrpsSorted < occ
+  -- Does not meet occurrence quota, no cards consumed
+        then (cs, Nothing)
+        else
+          let highestGrpNum =
+                if (null validGrpsSorted) || (null $ head validGrpsSorted)
+                  then Nothing
+                  else Just (fst . head . head $ validGrpsSorted)
+              (grpsToConsumeFrom, grpsIgnored) = splitAt occ validGrpsSorted
+              -- Only consume the first <sz> cards from the first <occ> groups
+              grpsConsumed = map (drop sz) grpsToConsumeFrom
+              remainingCards = (mconcat grpsConsumed) ++ mconcat (grpsIgnored)
+          in  (remainingCards, highestGrpNum)
+
+sameSuit :: [Card] -> Bool
+sameSuit [] = True
+sameSuit cs = all cmpSuit cs where cmpSuit c = snd c == snd (head cs)
+
+cmpNum :: Card -> Card -> Bool
+cmpNum a b = fst a == fst b
+
+cmpGrp :: [Card] -> [Card] -> Ordering
+cmpGrp as bs = cmpVal (head as) (head bs)
+
+cmpVal :: Card -> Card -> Ordering
+cmpVal = comparing fst
+
+highestVal :: [Card] -> Int
+highestVal = fst . maximumBy cmpVal
+
 -- In the card game poker, a hand consists of five cards and are ranked, from lowest to highest, in the following way:
 
 -- High Card: Highest value card.
@@ -43,94 +175,3 @@
 -- The file, poker.txt, contains one-thousand random hands dealt to two players. Each line of the file contains ten cards (separated by a single space): the first five are Player 1's cards and the last five are Player 2's cards. You can assume that all hands are valid (no invalid characters or repeated cards), each player's hand is in no specific order, and in each hand there is a clear winner.
 
 -- How many hands does Player 1 win?
-
-import Data.Ord ( comparing )
-import Data.List ( maximumBy, groupBy )
-
-data HighCard      = HC { hcVal::Int }
-data OnePair       = OP { opVal::Int }
-data TwoPairs      = TP { tpVal::Int }
-data ThreeKind     = TK { tkVal::Int }
-data Straight      = S  {  sVal::Int }
-data Flush         = F  {  fVal::Int }
-data FullHouse     = FH { fhVal::Int }
-data FourKind      = FK { fkVal::Int }
-data StraightFlush = SF { sfVal::Int }
-data RoyalFlush    = RF { rfVal::Int }
-
-type Card = (Int, Char)
-
-main :: IO ()
-main = print "54.hs"
-
--- solve :: Int 
-
-highestCard :: [Card] -> Int
-highestCard = fst . maximumBy cmpVal
-
-onePair :: [Card] -> Maybe Int
-onePair = getGrp 1 2
-
-twoPairs :: [Card] -> Maybe Int
-twoPairs = getGrp 2 2
-
-threeKind :: [Card] -> Maybe Int
-threeKind = getGrp 1 3
-
-flush :: [Card] -> Maybe ()
-flush cs = if (not . sameSuit) cs
-             then Nothing
-             else Just ()
-
--- Return highest num value
-straight :: [Card] -> Maybe Int
-straight cs = let nums = map fst cs
-                  minNum = minimum nums
-                  expected = sum [minNum .. minNum + 4]
-              in  if sum nums /= expected
-                    then Nothing
-                    else Just $ maximum nums
-
-fullHouse :: [Card] -> Maybe (Int, Int)
-fullHouse cs = pure (,) <*> threeKind cs <*> onePair cs
-
-fourKind :: [Card] -> Maybe Int
-fourKind = getGrp 1 4
-
-straightFlush :: [Card] -> Maybe Int
-straightFlush cs = flush cs >> straight cs
-
--- TODO:
-royalFlush :: [Card] -> Maybe ()
-royalFlush cs = let nums = map fst cs
-                    expected = 60 -- Expect 10 - Ace (J = 11, Q = 12, K = 13, A = 14)
-                    numsSatisfied = if sum nums /= expected
-                                      then Nothing
-                                      else Just ()
-                in flush cs >> numsSatisfied
-
---- helpers --
-
--- If no group found, returns Nothing
--- Otherwise returns the highest number in any of the groups
-getGrp :: Int -> Int -> [Card] -> Maybe Int
-getGrp occ sz cs = if length (groupByNum cs) < occ
-                     then Nothing
-                     else Just (maxNum . filter isLongEnough $ groupByNum cs)
-                     where groupByNum = groupBy cmpNum
-                           isLongEnough cs' = length cs' >= sz
-                           maxNum = fst . head . maximumBy cmpGrp
-
-sameSuit :: [Card] -> Bool
-sameSuit [] = True
-sameSuit cs = all cmpSuit cs
-              where cmpSuit c = snd c == snd (head cs)
-
-cmpNum :: Card -> Card -> Bool
-cmpNum a b = fst a == fst b
-
-cmpGrp :: [Card] -> [Card] -> Ordering
-cmpGrp as bs = cmpVal (head as) (head bs)
-
-cmpVal :: Card -> Card -> Ordering
-cmpVal = comparing fst
